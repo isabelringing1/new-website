b0053c5568bedbc028f9569e2a87e447be13e238
farmerisabel <isabel@doubleloopgames.com>
2021-09-27 18:01:33 -0700

    added all cards

--- main.js
+++ main.js
@@ -1,38 +1,120 @@
 var data;
+var map;
+var cardPositions = [];
+var windowWidth;
+
+var cardsPerRow;
+var cardContainerMinWidth = 450;
+var cardContainerHeight = 585;
 
 function getProjectData(){
     return $.getJSON('projects.json', function(jsonData, status, xhr){
         data = jsonData;
-        data.projects.sort((a, b) => (a.index < b.index) ? 1 : -1)
+        map = new Map();
+        for (var i = 0; i < data.projects.length; i++){
+            map.set(data.projects[i].hash, i);
+        }
+        data.projects.sort((a, b) => (a.index < b.index) ? 1 : -1);
     });
 }
 
 
 function renderProject(projectIndex){
-    document.getElementById('project').style.display = "block";
-    document.getElementById('cards').style.display = "none";
+    $('#project')[0].style.display = "block";
+    $('#cards')[0].style.display = "none";
 
-    var projectData = data.projects[projectIndex];
+    var projectData = data.projects[data.projects.length - projectIndex - 1];
 
     fetch('project.mustache')
     .then((response) => response.text())
     .then((template) => {
         var rendered = Mustache.render(template, projectData);
-        document.getElementById('project').innerHTML = rendered;    
+        $('#project')[0].innerHTML = rendered;    
     });
 
 }
 
 function renderCards(){
-    document.getElementById('cards').style.display = "flex";
-    document.getElementById('project').style.display = "none";
+    $('#cards')[0].style.display = "flex";
+    $('#project')[0].style.display = "none";
 
     fetch('cards.mustache')
     .then((response) => response.text())
     .then((template) => {
         var rendered = Mustache.render(template, data);
-        document.getElementById('cards').innerHTML = rendered;    
+        $('#cards')[0].innerHTML = rendered;
+        $('.card').on('click', (event) => {
+            card = event.target;
+            while (card.className != "card-container"){
+                card = card.parentNode;
+            }
+            console.log(card);
+            renderProject(parseInt(card.id));
+        });
+        var cardContainers = $('.card-container');
+        var cardContainerWidth = windowWidth / cardsPerRow;
+        var currX = 30;
+        var currY = 30;
+        for (var i = 0; i < cardContainers.length; i++){
+            cardContainer = cardContainers[i];
+            cardContainer.style.marginLeft = currX;
+            cardContainer.style.marginTop = currY;
+            cardPositions[i] = [currX, currY];
+            //console.log("card #" + i + " hashed to pos " + [currX, currY]);
+
+            if ((i + 1) % cardsPerRow == 0){
+                currX = 30;
+                currY += cardContainerHeight;
+            }
+            else{
+                currX += cardContainerWidth;
+            }
+        }
     });
+}
+
+function showTags(tags){
+    var shownCards = [];
+    for (var i = 0; i < data.projects.length; i++){
+        var proj = data.projects[i];
+        var included = false;
+        for (var j = 0; j < proj.tags.length && !included; j++){
+            for (var k = 0; k < tags.length && !included; k++){
+                if (proj.tags[j] === tags[k]){
+                    included = true;
+                }
+            }
+        }
+        if (!included){
+            var el = $('#' + proj.index)[0];
+            el.style.opacity = "0";
+            setDisplayNoneTimeout(el);
+        }
+        else{
+            shownCards.push(proj.index);
+        }
+    }
+
+    for (var i = 0; i < shownCards.length; i++){
+        $('#' + shownCards[i])[0].style.marginLeft = cardPositions[i][0] + "px";
+        $('#' + shownCards[i])[0].style.marginTop = cardPositions[i][1] + "px";
+    }
+}
+
+function setDisplayNoneTimeout(el){
+    setTimeout(function() {
+        el.style.display = "none";
+    }, 250);
+}
+
+function resetTags(){
+    var cards = $('.card-container');
+    for (var i = 0; i < cards.length; i++){
+        cards[i].style.display = "block";
+        cards[i].style.opacity = "1";
+        cards[i].style.marginLeft = cardPositions[i][0] + "px";
+        cards[i].style.marginTop = cardPositions[i][1] + "px";
+    }
 }
 
 window.fadeIn = function(obj) {
@@ -42,8 +124,18 @@ window.fadeIn = function(obj) {
 
 // On Page Load
 $(function() {
+    windowWidth = $(window).width();
+    cardsPerRow = Math.floor(windowWidth / cardContainerMinWidth);
+    console.log("cardsPerRow is " + cardsPerRow);
     getProjectData().then(() => {
-        renderCards();
-        //renderProject(1);
+        console.log(map);
+        console.log(map.get(window.location.hash));
+        if (window.location.hash && map.get(window.location.hash) != undefined){
+            console.log(window.location.hash);
+            renderProject(map.get(window.location.hash));
+        }
+        else{
+            renderCards();
+        }
     });
 });
